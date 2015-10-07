fd02b1cc2d24edcc93ad19a781b56ad828e70028
FluxIX <chris.d.herrick@gmail.com>
2015-10-07 00:08:46 -0500

    Formally renamed the library.

--- dotNET/src/Collections/Generic/Graph/Graph.cs
+++ dotNET/src/Collections/Generic/Graph/Graph.cs
@@ -1,20 +1,20 @@
 ﻿/* *
  * Copyright (C) 2011, 2012 Christopher Herrick
  * 
- * This file is part of the Flux Graph library.
+ * This file is part of the FluxLib library.
  *
- * The Flux Graph library is free software: you can redistribute it and/or modify
+ * The FluxLib library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
 
- * The Flux Graph library is distributed in the hope that it will be useful,
+ * The FluxLib library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the Flux Graph library.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the FluxLib library.  If not, see <http://www.gnu.org/licenses/>.
  * */
 
 using System;
